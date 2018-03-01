05c238f7b5b2a2c5bb8804f73c4fdcc628c52ba4
赵鑫磊 <jagen.zhao@gmail.com>
2018-03-01 11:35:33 +0800

    optimization code

--- tree/bstree/bstree.h
+++ tree/bstree/bstree.h
@@ -1,3 +1,19 @@
+/*
+ * Copyrigth (C) Jagen Zhao (2018) jagen.zhao@gmail.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This porgram is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY of FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details.
+ * 
+ * You should hava received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/license/>.
+ */
 #ifndef __BSTREE_H__
 #define __BSTREE_H__
 
